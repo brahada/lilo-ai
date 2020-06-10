14b8d9f2787665d971f3e27771f6770d360f7b41
Sanchit Pandey <sanchitpandey72@gmail.com>
2020-06-10 23:35:12 +0530

    Made the loop

--- bot.py
+++ bot.py
@@ -4,9 +4,12 @@ API_ENDPOINT = 'https://api.wit.ai/message'
 WIT_ACCESS_TOKEN = 'DJP3NJSD236J77UO3MFXPROK2RRJ2A6G'
 
 headers = {'Authorization': 'Bearer {}'.format(WIT_ACCESS_TOKEN)}
-query = {'q': 'Hi'}
+q = ""
+while (q != "q"):
+      q = input("Ask?")
+      query = {'q': q}
 
-resp = requests.get(API_ENDPOINT, headers=headers, params=query)
-data = resp.json()
+      resp = requests.get(API_ENDPOINT, headers=headers, params=query)
+      data = resp.json()
 
-print(data)
+      print(data['text'])
